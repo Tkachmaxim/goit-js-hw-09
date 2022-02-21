72ca20e9d622be186eabb705dc22af43bf89b73c
Tkachmaxim <tkachmaxim23@gmail.com>
2022-02-21 16:53:47 +0200

    d

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delay: document.querySelector('.form').delay,
+  step: document.querySelector('.form').step,
+  amount: document.querySelector('.form').amount,
+  submitButton: document.querySelector('.form button'),
+};
+
+refs.submitButton.addEventListener('click', onCreateButton);
+
+function onCreateButton(event) {
+  event.preventDefault();
+  const amount = Number(refs.amount.value);
+  let delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  for (let index = 1; index <= amount; index++) {
+    createPromise(index, delay).then(Notify.success('Yes')).catch(Notify.failure('No'));
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promiseInstance = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, resolve });
+      } else {
+        reject({ position, resolve });
+      }
+    }, delay);
+  });
+
+  return promiseInstance;
 }
